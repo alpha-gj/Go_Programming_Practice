466ab6830339b2255dbddf4afb889aaf66932059
alpha-gj <Guang-Jie_Jhang@alphanetworks.com>
2018-09-29 15:20:40 +0800

    cal_string_len for english & chinese

---
+++ src_practice/String_operation/cal_string_len/cal_string_len.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+import "unicode/utf8"
+
+func main() {
+
+	ch := "生活大爆炸"
+	en := "life"
+
+
+	fmt.Printf("string is %s, data type is %T, and string len is %d by len(%s)\n", en, ch, len(en), en)
+	fmt.Printf("string is %s, data type is %T, and string len is %d by len(%s)\n", ch, ch, len(ch), ch)
+	fmt.Printf("\n")
+	fmt.Printf("\n")
+	fmt.Printf("Using the len() to cal Unicode is not ok, beacasue one chinese word allocates 3 byte\n")
+	fmt.Printf("it should use utf8.RuneCountInString\n")
+	fmt.Printf("string is %s, data type is %T, and the string len is %d by utf8.RuneCountInString\n", ch, ch, utf8.RuneCountInString(ch))
+
+
+}
