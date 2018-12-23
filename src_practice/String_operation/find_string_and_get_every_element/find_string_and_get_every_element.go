14700ad3322bf4f8d8b0c6d6851bc8dbfec5dad0
alpha-gj <Guang-Jie_Jhang@alphanetworks.com>
2018-12-23 13:37:15 +0800

    [find_string_and_get_every_element] add new practice

--- src_practice/String_operation/find_string_and_get_every_element/find_string_and_get_every_element.go
+++ src_practice/String_operation/find_string_and_get_every_element/find_string_and_get_every_element.go
@@ -1,35 +1,30 @@
 package main
 
 import "fmt"
+import "unicode/utf8"
 
 func main() {
+
 	thmem := "生活大爆炸 start"
-	fmt.Println("Using the ascii")
+	fmt.Printf("Printf the theme is %s, and printf every char for it\n", thmem);
+
+	// Using
+	fmt.Printf("# Using index\n");
 	for i := 0; i < len(thmem); i++ {
-		fmt.Printf("ascii: %c %d\n", thmem[i], thmem[i])
+		fmt.Printf("ascii: %c %d\n", thmem[i], thmem[i]);
 	}
+	fmt.Printf("len of thmem is %d (it is wrong len), by using len(thmem)\n", len(thmem));
 
-	fmt.Println("")
-	for index, s := range thmem {
-		fmt.Printf("Unicode: %c %d\n", s, s)
-		fmt.Println(index)
-	}
+	fmt.Printf("\n\n");
 
-	for index, s := range thmem {
-		if index == 0 {
-			fmt.Printf("The index of %d is %c\n", index, s)
-		}
-		if index == 3 {
-			fmt.Printf("The index of %d is %c\n", index, s)
-		}
-		if index == 6 {
-			fmt.Printf("The index of %d is %c\n", index, s)
-		}
-		if index == 9 {
-			fmt.Printf("The index of %d is %c\n", index, s)
-		}
-		if index == 12 {
-			fmt.Printf("The index of %d is %c\n", index, s)
-		}
+	fmt.Printf("# Using range\n");
+	for i, s := range thmem {
+		fmt.Printf("i is %d, s is %c %d\n", i, s, s);
 	}
+	fmt.Printf("len of thmem is %d (it is correct), by using utf8.RuneCountInString(thmem)\n", utf8.RuneCountInString(thmem));
+
+	for i, c := range "go" {
+        fmt.Println(i, c)
+		fmt.Printf("c is %c\n", c);
+    }
 }
