926b738e42a458bbb065a0f2c118c75bec9f006d
alpha-gj <Guang-Jie_Jhang@alphanetworks.com>
2018-12-23 14:27:30 +0800

    [get_one_char_in_string] add new comment

--- src_practice/String_operation/get_one_char_in_string/get_one_char_in_string.go
+++ src_practice/String_operation/get_one_char_in_string/get_one_char_in_string.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 import "strings"
+import "unicode/utf8"
 
 //using strings.Index to find substring
 
@@ -17,4 +18,9 @@ func main() {
 
 	fmt.Println(comma, pos, tracer[comma+pos:])
 	fmt.Println(comma, pos, tracer[comma:])
+
+	//sol. 
+	//死    神    來    了      ,        死       神       b  y  e  b  y  e
+	//0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26
+	fmt.Printf("len of tracer is %d\n", utf8.RuneCountInString(tracer))
 }
