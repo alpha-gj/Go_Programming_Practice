48ebb9b7f0439c835ebba22c42b2507d4945ab50
alpha-gj <Guang-Jie_Jhang@alphanetworks.com>
2018-12-23 14:19:00 +0800

    [get_one_char_in_string] add new practice

--- src_practice/String_operation/get_one_char_in_string/get_one_char_in_string.go
+++ src_practice/String_operation/get_one_char_in_string/get_one_char_in_string.go
@@ -6,13 +6,15 @@ import "strings"
 //using strings.Index to find substring
 
 func main() {
+	//NOTE there are two blanks after comma(,)
 	tracer := "死神來了,  死神bye bye"
 
-	comma := strings.Index(tracer, ", ")
+	comma := strings.Index(tracer, ",")
 	fmt.Printf("comma is %d\n", comma)
 
 	pos := strings.Index(tracer[comma:], "死神")
 	fmt.Printf("pos is %d\n", pos)
 
 	fmt.Println(comma, pos, tracer[comma+pos:])
+	fmt.Println(comma, pos, tracer[comma:])
 }
